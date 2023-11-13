dad457d27d9c3737f84f5c7a9d890af1d699eeed
nmrenyi <renyi1006@gmail.com>
2023-11-13 09:52:20 +0800

    feat: add url, title, thumbnail_url

--- download.py
+++ download.py
@@ -1,7 +1,15 @@
 import pytube
 
 def main():
-    pass
+    url = 'https://www.youtube.com/watch?v=Ed4WOs29E08'
+    yt = pytube.YouTube(url)
+    title = yt.title
+    print(title)
+    thumbnail_url = yt.thumbnail_url
+    print(thumbnail_url)
+    print(yt.streams)
+
+
 
 if __name__ == '__main__':
     main()
