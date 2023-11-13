e5ad99b0cead385d5ef31efd4d807170e770ea57
nmrenyi <renyi1006@gmail.com>
2023-11-13 10:30:24 +0800

    feat: add combine audio and video

--- combine.py
+++ combine.py
@@ -1,8 +1,11 @@
 import ffmpeg
 
 def main():
-    pass
+    input_video = ffmpeg.input('video.mp4')
+    input_audio = ffmpeg.input('audio.mp4')
+    output_name = 'output.mp4'
 
+    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(output_name).run()
 
 if __name__ == '__main__':
     main()
