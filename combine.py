ec2a00f920713f36f5cde79558fbe947eebb171a
nmrenyi <renyi1006@gmail.com>
2023-11-13 11:10:35 +0800

    feat: add asychronized ffmpeg

--- combine.py
+++ combine.py
@@ -4,7 +4,7 @@ def combine(input_video_path, input_audio_path, output_path):
     input_video = ffmpeg.input(input_video_path)
     input_audio = ffmpeg.input(input_audio_path)
 
-    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(output_path).run()
+    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(output_path).run_async()
 
 if __name__ == '__main__':
     combine()
